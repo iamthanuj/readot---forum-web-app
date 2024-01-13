6b10a55d03ed3e768fcc011a5a376086d0c2b3f4
iamthanuj <thanujdevil@gmail.com>
2024-01-13 21:44:18 +0530

    connecting DB

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,33 +1,30 @@
-const getPosts = (req, res)=>{
-    res.status(200).json({message:'Posts'})
-}
+const asyncHandler = require("express-async-handler");
 
+const getPosts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Posts" });
+});
 
-const createPost = (req, res)=>{
+const createPost = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text");
+  }
 
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a text')
-    }
+  console.log(req.body);
+  res.status(200).json({ message: req.body });
+});
 
+const updatePost = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `${req.params.id} is updated` });
+});
 
-    console.log(req.body)
-    res.status(200).json({message:req.body})
-}
-
-
-const updatePost = (req, res)=>{
-    res.status(200).json({message:`${req.params.id} is updated`})
-}
-
-
-const deletePost = (req, res)=>{
-    res.status(200).json({message:`${req.params.id} is deleted`})
-}
+const deletePost = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `${req.params.id} is deleted` });
+});
 
 module.exports = {
-    getPosts,
-    createPost,
-    updatePost,
-    deletePost,
-}
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+};
